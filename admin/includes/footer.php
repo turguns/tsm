6efa8b3c80e7ed90703948f08594f7909e1ae752
turgun <turguns@gmail.com>
2022-04-18 17:30:37 +0200

    fix

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>© 2022 T.Duishonaliev  |  <a href="#">KGNU</a> </p>
+</div>	
---
+++ test.php
@@ -0,0 +1,55 @@
+<html>
+ <body>
+
+ <div class="figure">
+  <img src="images/naryn.jpg"
+       alt="The head and torso of a dinosaur skeleton;
+            it has a large head with long sharp teeth"
+       width="400"
+       height="341">
+  <video width="400" controls>
+  <source src="audio/naryn.mp4" type="audio/mp4" title="Naryn">
+  Your browser does not support HTML video.
+  </video>
+  <p>Naryn</p>
+</div>
+
+<div class="figure">
+  <img src="images/naryn.jpg"
+       alt="The head and torso of a dinosaur skeleton;
+            it has a large head with long sharp teeth"
+       width="400"
+       height="341">
+  <video width="400" controls>
+  <source src="audio/naryn.mp4" type="audio/mp4" title="Naryn">
+  Your browser does not support HTML video.
+  </video>
+  <p>Bir Nerse</p>
+</div>
+
+
+
+
+  <p>NARYN 
+
+  <img src="images/naryn.jpg">    
+  <video width="400" controls>
+  <source src="audio/naryn.mp4" type="audio/mp4" title="Naryn">
+  Your browser does not support HTML video.
+  </video>
+</p>
+  <p> Birnerse 
+  
+  <video width="400" controls>
+  <source src="audio/naryn.mp4" type="audio/mp4" title="Naryn">
+  Your browser does not support HTML video.
+  </video>
+  </p>
+
+
+
+  
+ </body>
+</html>
+
+
